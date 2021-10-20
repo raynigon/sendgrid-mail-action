46f26ee4de19acf4b1fda56aa0ded7e371bf4ea4
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-10-20 09:06:03 +0200

    Enable E-Mail Attachments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,26 @@ import * as core from '@actions/core';
 import MailService from '@sendgrid/mail';
 import { MailDataRequired } from "@sendgrid/helpers/classes/mail";
 import { EmailData } from "@sendgrid/helpers/classes/email-address";
+import * as mime from "mime-types";
+import fs from "fs";
+import path from "path"
+
+async function createAttachment(filepath: string) {
+    const filename = path.parse(filepath).base
+    let mimeType = mime.lookup(filename)
+    if (!mimeType) {
+        mimeType = "text/plain"
+    }
+    const buffer = await fs.promises.readFile(filepath)
+    const base64 = buffer.toString('base64')
+    return {
+        content: base64,
+        filename: filename,
+        type: mimeType,
+        disposition: 'attachment',
+        content_id: filename,
+    }
+}
 
 async function main() {
     // Input
@@ -11,6 +31,7 @@ async function main() {
     const inputSubject = core.getInput('subject');
     const inputText = core.getInput('text');
     const inputHtml = core.getInput('html');
+    const inputAttachments = core.getInput('attachments');
 
     if (inputToken === undefined) {
         throw new Error('Undefined token');
@@ -18,14 +39,21 @@ async function main() {
     if (inputTo === undefined) {
         throw new Error('Undefined receivers');
     }
-    if (inputText === "" && inputHtml === ""){
+    if (inputText === "" && inputHtml === "") {
         throw new Error('Either Text or HTML is required');
     }
 
     MailService.setApiKey(inputToken);
 
     const receivers: EmailData[] = inputTo.split(",")
-        .map(receiver => receiver.replace("\n", "").replace("\r", "").replace("\t", "").trim());
+        .map((receiver: string) => receiver.replace("\n", "").replace("\r", "").replace("\t", "").trim());
+
+    const attachments = await Promise.all(
+        inputAttachments.split(",")
+            .map((filename: string) => filename.trim())
+            .filter((filename: string) => filename != "")
+            .map(createAttachment)
+    )
 
     const mailData = {
         to: receivers,
@@ -33,6 +61,7 @@ async function main() {
         subject: inputSubject,
         text: inputText !== "" ? inputText : undefined,
         html: inputHtml !== "" ? inputHtml : undefined,
+        attachments: attachments,
     };
 
     MailService.sendMultiple(mailData as MailDataRequired)
